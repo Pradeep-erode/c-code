91efb64afd28e9d602c28e53beac8fed45669531
pradeep <79890291+Pradeep-erode@users.noreply.github.com>
2021-05-07 11:38:19 +0530

    Update Abstraction.cs

--- Abstraction.cs
+++ Abstraction.cs
@@ -1,6 +1,8 @@
 Abstraction program 
 
-///////////////main class
+
+//main class
+
 
 using System;
 
@@ -22,7 +24,10 @@ namespace ConsoleApp1
     }
 }
 
-///////////////class2
+
+
+//father class
+
 
 using System;
 using System.Collections.Generic;
@@ -32,6 +37,52 @@ namespace ConsoleApp1
 {
     //abstract can have both  marked abstract method and non-abstract method in abstract class
     //But interface class having abstract method only 
+
+    public abstract class father
+    {
+        //abstract method(or marked abstract method) and implemention not done
+        public abstract void display();
+
+
+        //non-abstract method in abstract class 
+        public void show(int a,int b)//getting inputs through arguments
+        {
+           
+            Console.Write("\nsum =" + (a + b));
+        }
+    }
+
+
+}
+
+
+//child class
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //inheritance of father class
+    public class Child:father
+    {
+        
+        //implementation of abstract class using override keyword whereas in method overriding this is not compulsary 
+        //to implement 
+
+
+        public override void display()
+        {
+            Console.WriteLine("I am implanted class");
+        }
+        public void disk()
+        {
+            Console.Write("I am pradeep");
+        }
+    }
+}
 
     public abstract class father
     {
